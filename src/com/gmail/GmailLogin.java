0c00c5db53a7b60aaa8f8f3237cc80ee34adffa8
ashokreddy <sareddy789@gmail.com>
2018-03-18 17:21:27 +0530

    display method added

--- src/com/gmail/GmailLogin.java
+++ src/com/gmail/GmailLogin.java
@@ -4,6 +4,9 @@ public class GmailLogin {
 	public void launch(){
 		System.out.println("ashokreddy");
 	}
+	public void display(){
+		System.out.println("singareddy");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
