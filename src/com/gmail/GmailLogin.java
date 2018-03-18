940151485e39ea60623f416604737817683141cd
ashokreddy <sareddy789@gmail.com>
2018-03-18 16:59:14 +0530

    modified

--- src/com/gmail/GmailLogin.java
+++ src/com/gmail/GmailLogin.java
@@ -1,6 +1,9 @@
 package com.gmail;
 
 public class GmailLogin {
+	public void launch(){
+		System.out.println("ashokreddy");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
